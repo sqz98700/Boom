384fe2f4405f238718970a08b105962f6a2c7e0e
cheese <sbb0779@163.com>
2018-08-31 16:18:28 +0800

    16.18

--- Design/src/Main.java
+++ Design/src/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(new Date());  
-        
-        
+        System.out.println(new Date());
 
     }
 }
