ced40a06daaf2f94fee9df183da3fa23fde38a34
cheese <sbb0779@163.com>
2018-08-31 14:56:40 +0800

    tianlalu

--- Design/src/com/pattern/Factory/ShapeFactory.java
+++ Design/src/com/pattern/Factory/ShapeFactory.java
@@ -8,7 +8,6 @@ public class ShapeFactory {
     public Shape getShape(String shapeType){
         if(shapeType == null){
             return null;
-
         }
         if(shapeType.equalsIgnoreCase("CIRCLE")){
             return new Circle();
